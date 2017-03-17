923a76ec615bf8d83b6029d9b62474964d0aafcb
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2017-03-17 12:03:02 -1000

    Add backend.

--- src/loquat_sup.erl
+++ src/loquat_sup.erl
@@ -1,9 +1,25 @@
-%%%-------------------------------------------------------------------
-%% @doc loquat top level supervisor.
-%% @end
-%%%-------------------------------------------------------------------
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2017 Christopher S. Meiklejohn.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
 
 -module(loquat_sup).
+-author("Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>").
 
 -behaviour(supervisor).
 
@@ -28,7 +44,12 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+    PlumtreeBackend = {loquat_plumtree_backend,
+                       {loquat_plumtree_backend, start_link, []},
+                        permanent, 5000, worker,
+                        [loquat_plumtree_backend]},
+
+    {ok, { {one_for_all, 0, 1}, [PlumtreeBackend]} }.
 
 %%====================================================================
 %% Internal functions
